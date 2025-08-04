5228e213147028bf578c91babe96b2afa5ef8846
wwx1442096 <wangyang574@h-partners.com>
2025-08-04 11:17:58 +0800

    feat(*): 实现Redis分布式锁，满足互斥、避免死锁、不解锁其他客户端、可重入等特性（为满足锁续期，需要实现看门狗机制）

--- src/main/java/com/myproject/prescription/enums/BizExceptionEnum.java
+++ src/main/java/com/myproject/prescription/enums/BizExceptionEnum.java
@@ -11,6 +11,9 @@ public enum BizExceptionEnum {
     PRESCRIPTION_DRUGS_NOT_EXIST(new PrescriptionBizException("1000_2", "处方单药品为空")),
 
     PRESCRIPTION_FULFILL_STATUS_ERROR(new PrescriptionBizException("2000_1", "处方单履约状态异常")),
-    PRESCRIPTION_TOKEN_EXPIRED_ERROR(new PrescriptionBizException("3000_1", "处方单重复创建"));
+    PRESCRIPTION_TOKEN_EXPIRED_ERROR(new PrescriptionBizException("3000_1", "处方单重复创建")),
+
+    RELEASE_OTHER_LOCK_ERROR(new PrescriptionBizException("4000_1", "释放锁异常（当前客户端不持有锁）"))
+    ;
     private final PrescriptionBizException exception;
 }
--- src/main/java/com/myproject/prescription/lock/RedisDistributeLock.java
+++ src/main/java/com/myproject/prescription/lock/RedisDistributeLock.java
@@ -1,21 +1,84 @@
 package com.myproject.prescription.lock;
 
+import com.myproject.prescription.enums.BizExceptionEnum;
+import com.myproject.prescription.utils.AssertUtils;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
 /**
- * TODO 基于Redis实现分布式锁
+ * 基于Redis实现分布式锁
  */
-public class RedisDistributeLock implements Lock{
+@Slf4j
+@RequiredArgsConstructor
+public class RedisDistributeLock implements Lock {
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    private static final String UNLOCK_SCRIPT = "if redis.call('get', KEYS[1]) == ARGV[1] then return redis.call('del', KEYS[1]) else return 0 end";
+
+    private static ThreadLocal<Map<String, Integer>> lockCounts = ThreadLocal.withInitial(HashMap::new);
+
+    private final String instanceId = UUID.randomUUID().toString();
+
     @Override
     public boolean tryLock(String key) {
+        Map<String, Integer> lockCntMap = lockCounts.get();
+        Integer lockCnt = lockCntMap.get(key);
+        if (lockCnt != null) {
+            lockCntMap.put(key, ++lockCnt);
+            return true;
+        }
+        String lockVal = instanceId + ":" + Thread.currentThread().getId();
+        Boolean set = redisTemplate.opsForValue().setIfAbsent(key, lockVal, 90000, TimeUnit.MILLISECONDS);
+        if (Boolean.TRUE.equals(set)) {
+            lockCntMap.put(key, 1);
+            return true;
+        }
         return false;
     }
 
     @Override
     public void lock(String key) {
-
+        Map<String, Integer> lockCntMap = lockCounts.get();
+        Integer lockCnt = lockCntMap.get(key);
+        if (lockCnt != null) {
+            lockCntMap.put(key, ++lockCnt);
+            return;
+        }
+        while (!tryLock(key)) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                log.warn("Lock failed, cause: ", e);
+                if (Thread.interrupted()) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
     }
 
     @Override
     public void release(String key) {
+        Map<String, Integer> lockCntMap = lockCounts.get();
+        Integer lockCnt = lockCntMap.get(key);
+        // TODO 优化异常，抽象锁，区分业务异常和系统异常
+        AssertUtils.notNull(lockCnt, BizExceptionEnum.RELEASE_OTHER_LOCK_ERROR.getException());
+
+        lockCnt--;
+        if (lockCnt < 1) {
+            lockCntMap.remove(key);
+            DefaultRedisScript<Long> script = new DefaultRedisScript<>(UNLOCK_SCRIPT, Long.class);
+            redisTemplate.execute(script, Collections.singletonList(key), instanceId + Thread.currentThread().getId());
+        } else {
+            lockCntMap.put(key, lockCnt);
+        }
 
     }
 }
